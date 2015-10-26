ff2e9759bed118ba39863819b6281858be9c6cde
ashwinkarthik <ashwinkarthik.srinivasan@stonybrook.edu>
2015-10-25 21:54:23 -0400

    Playlist-app

---
+++ app.js
@@ -0,0 +1,85 @@
+
+var playlistApp = angular.module('playlistApp',[])
+.config(function($sceDelegateProvider) {
+        $sceDelegateProvider.resourceUrlWhitelist([
+                                                   'self',
+                                                   '*://www.youtube.com/**'
+                                                   ]);
+        });
+
+
+playlistApp.controller('mainController',function($scope,$http){
+		var vm = this;
+		vm.artists = [ 'Calvin Harris','AR Rahman','Ilayaraja','Anirudh','Taylor Swift','Ed Sheeran','Rihanna','David Guetta','Adele'];
+        vm.selectedArtist = vm.artists[0];
+        vm.results= [];
+        getResults(vm.selectedArtist);
+        
+        $scope.changedArtist = function(selectedArtist){
+                       vm.selectedArtist = selectedArtist;
+                       vm.results = getResults(vm.selectedArtist);
+                       
+        }
+        
+        $scope.getURL = function (id) {
+                       return 'https://www.youtube.com/embed/'+id+'?rel=0'
+        }
+                       
+        $scope.showModal = false;
+        $scope.selectedResult =  null;
+        $scope.toggleModal = function(selectedResult){
+                       $scope.showModal = !$scope.showModal;
+                      $scope.selectedResult = selectedResult;
+        };
+
+        function getResults(selectedArtist){
+                       $http.get("http://localhost:1500/"+selectedArtist).success(function(response){
+                                                                                  vm.results = response;                                             });
+        }
+            
+	});
+
+
+playlistApp.directive('modal', function () {
+                  return {
+                  template: '<div class="modal fade">' +
+                  '<div class="modal-dialog" >' +
+                  '<div class="modal-content" width="800">' +
+                  '<div class="modal-header">' +
+                  '<button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>' +
+                  '<h4 class="modal-title">{{ title }}</h4>' +
+                  '</div>' +
+                  '<div id="player-modal" class="modal-body" ng-transclude></div>' +
+                  '</div>' +
+                  '</div>' +
+                  '</div>',
+                  restrict: 'E',
+                  transclude: true,
+                  replace:true,
+                  scope:true,
+                  link: function postLink(scope, element, attrs) {
+                  scope.title = attrs.title;
+                  
+                  scope.$watch(attrs.visible, function(value){
+                               if(value == true)
+                               $(element).modal('show');
+                               else
+                               $(element).modal('hide');
+                               });
+                  
+                  $(element).on('shown.bs.modal', function(){
+                                scope.$apply(function(){
+                                             scope.$parent[attrs.visible] = true;
+                                             });
+                                });
+                  
+                  $(element).on('hidden.bs.modal', function(){
+                                $("#player-modal iframe").attr("src", $("#player-modal iframe").attr("src"));
+                                scope.$apply(function(){
+                                             scope.$parent[attrs.visible] = false;
+                                             });
+                                });
+                  }
+                  };
+                  });
+
---
+++ server.js
@@ -0,0 +1,55 @@
+ 
+
+var express = require('express'),
+    path = require('path'),
+    app = express(),
+    googleapis = require('googleapis'),
+    API_KEY = 'AIzaSyBsa8KVrYKsCi2PjXAkTlz5DavoKQceNRQ',
+    RESULT_COUNT = 20;
+
+googleapis.options ({ auth: API_KEY });
+var youtube = googleapis.youtube ('v3');
+
+app.use(express.static('../playlist\-app/'));
+
+app.get('/',function(req,res){
+        res.sendFile('../playlist\-app/index.html');
+});
+
+app.get('/:name',function(req,res){
+        
+  youtube.search.list(
+        {
+         part: 'snippet',
+         type: 'video',
+         q: req.params.name,
+         maxResults: RESULT_COUNT,
+         order: 'date',
+         safeSearch: 'moderate',
+         videoEmbeddable: true
+        },
+        function(err,result){
+                if(err)
+                        console.log(err);
+                else{
+                      
+                        var results = [];
+                        for(var i=0;i<RESULT_COUNT;i++) {
+                            var item = result.items[i];
+                            var resultObject = { };
+                            resultObject.videoId = item.id.videoId;
+                            resultObject.title = item.snippet.title;
+                            resultObject.imageURL = item.snippet.thumbnails.high.url;
+                            results.push(resultObject);
+                        }
+                       
+                        console.log(JSON.stringify(results,null,2));
+                        res.setHeader('Content-Type', 'application/json');
+                        res.send(JSON.stringify(results,null,2));
+                }
+
+        });
+});
+
+app.listen(1500);
+console.log('Listening on Port 1500');
